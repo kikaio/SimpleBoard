1f23257002616df9f290cb90bbec92d8a67f3195
DESKTOP-NV48I2Q\user <dd>
2024-06-14 20:40:11 +0900

    remember me 기능 추가중

--- simpleboard/src/main/java/com/portfolio/simpleboard/config/CustomSecurityConfig.java
+++ simpleboard/src/main/java/com/portfolio/simpleboard/config/CustomSecurityConfig.java
@@ -17,9 +17,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatchers;
 
+import javax.sql.DataSource;
 import java.util.Arrays;
 import java.util.List;
 
@@ -28,6 +31,8 @@ import java.util.List;
 @RequiredArgsConstructor
 @Configuration
 public class CustomSecurityConfig {
+
+    private final DataSource dataSource;
 
     private final SimpleBoardUserDetailService simpleBoardUserDetailService;
 
@@ -58,9 +63,25 @@ public class CustomSecurityConfig {
         });
 
         http.formLogin(custom->{
-            custom.loginPage("/member/login");
+            custom.loginPage("/member/login")
+            ;
+        });
+        http.rememberMe(custom->{
+            custom
+                    .key("4312312421")
+                    .tokenRepository(persistentTokenRepository())
+                    .userDetailsService(simpleBoardUserDetailService)
+                    .tokenValiditySeconds(60 * 60 * 24 * 30)
+            ;
         });
         return http.build();
+    }
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
+        repo.setDataSource(dataSource);
+        return repo;
     }
 
 
--- simpleboard/src/main/java/com/portfolio/simpleboard/controller/SimpleBoardMemberController.java
+++ simpleboard/src/main/java/com/portfolio/simpleboard/controller/SimpleBoardMemberController.java
@@ -4,6 +4,7 @@ package com.portfolio.simpleboard.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,8 +17,10 @@ public class SimpleBoardMemberController {
     @GetMapping("/login")
     public String getLoginPage(String error, String logout) {
         log.info("getLoginPage called");
+
+        if(logout != null) {
+            log.info("logout!");
+        }
         return "/member/login";
     }
-
-
 }
